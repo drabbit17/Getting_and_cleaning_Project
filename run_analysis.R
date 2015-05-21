56d7af07837783e719b63ee45d822e25e6458007
drabbit17 <matteo.pallini17@gmail.com>
2015-05-21 18:47:37 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 # A) dataset set-up
 Url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(Url, destfile="~/data/R/coursera/getting_cleaning_data/project/dataset.zip", method="curl")
+download.file(Url, destfile = "~/data/R/coursera/getting_cleaning_data/project/dataset.zip", method = "curl")
 # manual file unzipping
 
 gTest <- read.table("~/data/R/coursera/getting_cleaning_data/project/Dataset/test/X_test.txt")
@@ -16,18 +16,18 @@ featNames <- read.table("~/data/R/coursera/getting_cleaning_data/project/Dataset
 
 # set up of the Test sample
 library(data.table)
-setnames(gTest, old=names(gTest), new=featNames[,2]) # rename different measures columns
-setnames(subTest, old=c("V1"), new=c("SubID")) # rename Subjects column
-setnames(actTest, old=c("V1"), new=c("Activity")) # rename Activities column
+setnames(gTest, old = names(gTest), new = featNames[,2]) # rename different measures columns
+setnames(subTest, old = c("V1"), new = c("SubID")) # rename Subjects column
+setnames(actTest, old = c("V1"), new = c("Activity")) # rename Activities column
 dim(gTest);dim(subTest);dim(actTest)
-test <- cbind(subTest, actTest, group=c(rep(0,2947)), gTest) # Subject identifier, activity identifier and the feature variables and clipped all toghether. An extra column called group allows for identifying test/train members
+test <- cbind(subTest, actTest, group = c(rep(0,2947)), gTest) # Subject identifier, activity identifier and the feature variables and clipped all toghether. An extra column called group allows for identifying test/train members
 dim(test)
 
-setnames(gTrain, old=names(gTrain), new=featNames[,2]) # rename different measures columns
-setnames(subTrain, old=c("V1"), new=c("SubID")) # rename Subjects column
-setnames(actTrain, old=c("V1"), new=c("Activity")) # rename Activities column
+setnames(gTrain, old = names(gTrain), new = featNames[,2]) # rename different measures columns
+setnames(subTrain, old = c("V1"), new = c("SubID")) # rename Subjects column
+setnames(actTrain, old = c("V1"), new = c("Activity")) # rename Activities column
 dim(gTrain);dim(subTrain);dim(actTrain)
-train <- cbind(subTrain, actTrain, group=c(rep(1,7352)), gTrain) # Same process followed for test
+train <- cbind(subTrain, actTrain, group = c(rep(1, 7352)), gTrain) # Same process followed for test
 dim(train)
 
 final <- rbind(test, train) # the test and the train datasets are merged together
@@ -35,7 +35,7 @@ final <- rbind(test, train) # the test and the train datasets are merged togethe
 #################################
 # C) 3) label activities in the dataset
 
-final$Activity <- factor(final$Activity, levels=c(1,2,3,4,5,6), labels=c("WALKING","WALKING-UPSTAIRS","WALKING-DOWNSTAIRS","SITTING","STANDING","LAYING"))
+final$Activity <- factor(final$Activity, levels = c(1, 2, 3, 4, 5, 6), labels = c("WALKING", "WALKING-UPSTAIRS", "WALKING-DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
 
 #################################
 # D) 2) subsample mean and std
@@ -47,7 +47,7 @@ final$Activity <- factor(final$Activity, levels=c(1,2,3,4,5,6), labels=c("WALKIN
 
 colNames <- c("SubID", "Activity", "group", as.character(featNames[,2]))
 names(final) <- gsub("[(),-]", "", colNames)
-subsample <- data.frame(final[1:2],final[((grepl("[Mm]ean",names(final))|grepl("std",names(final))))])
+subsample <- data.frame(final[1:2], final[((grepl("[Mm]ean", names(final))|grepl("std", names(final))))])
 
 #################################
 # E) 5) new dataset with averages
@@ -55,12 +55,12 @@ subsample <- data.frame(final[1:2],final[((grepl("[Mm]ean",names(final))|grepl("
 library(dplyr)
 # process to obtain the data frame including means in subgroups
 by_idSub <- group_by(subsample, SubID, Activity) # create the subgroups
-cols <- names(by_idSub)[-c(1,2)] # removes the column not including features
-test <- sapply(cols, function(x) substitute(mean(x), list(x=as.name(x))))
-finalSub <- data.frame(do.call(summarise, c(list(.data=by_idSub), test)))
+cols <- names(by_idSub)[-c(1, 2)] # removes the column not including features
+test <- sapply(cols, function(x) substitute(mean(x), list(x = as.name(x))))
+finalSub <- data.frame(do.call(summarise, c(list(.data = by_idSub), test)))
 
 # export the final dataset
-write.table(finalSub, "~/data/R/coursera/getting_cleaning_data/project/final.txt", row.name=FALSE)
+write.table(finalSub, "~/data/R/coursera/getting_cleaning_data/project/final.txt", row.name = FALSE)
 
 # alternative pipeline approach to obtain the new dataset
 altfinalSub <- subsample %>% group_by( SubID, Activity) %>%  summarise_each(funs(mean))
